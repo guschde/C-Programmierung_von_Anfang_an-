607c23abfbbe5768e8ecf58e570e44676eb16b30
guschde <andreas.guerster@gmail.com>
2018-05-25 23:20:13 +0200

    firstC

---
+++ bsp10001.c
@@ -0,0 +1,10 @@
+/* bsp10001.c */
+#include <stdio.h>
+main(){
+  printf("\nDies ist ein ersters C-Programm.\n");
+  printf("\nWie Sie sehen, kann 'printf' ");
+  printf("nicht nur Texte druckern,\n");
+  printf("sondern auch rechnen.\n\n");
+  printf("13 * 7 = %i \n", 13 * 7);
+}
+
